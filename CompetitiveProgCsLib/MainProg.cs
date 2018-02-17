752121dd6b685fac9ae914fff8bab94e6ffad15f
daiki <haru.td@gmail.com>
2018-02-17 21:48:49 +0900

    プロジェクトの追加

---
+++ CompetitiveProgCsLib/MainProg.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgCsLib
+{
+	class MainProg
+	{
+		static int In => int.Parse(Console.ReadLine());
+		static int[] Ins => Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+		static void Out(bool b) => Console.WriteLine(b ? "Yes" : "No");
+
+		static void Main(string[] args)
+		{
+		}
+	}
+}

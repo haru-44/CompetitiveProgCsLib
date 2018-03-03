ee7419422ab28cc8dd0e931590db8b6822c2647e
daiki <haru.td@gmail.com>
2018-03-03 20:57:51 +0900

    ExtensionMethods.csの追加

---
+++ CompetitiveProgCsLib/Utilities/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CompetitiveProgCsLib.Utilities
+{
+	/// <summary>
+	/// 拡張メソッドを提供する
+	/// </summary>
+	public static class ExtensionMethods
+	{
+		/// <summary>
+		/// Itemの数を集計する
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="items"></param>
+		/// <returns></returns>
+		public static Dictionary<T, int> NumberCount<T>(this IEnumerable<T> items)
+		{
+			return items.GroupBy(s => s).Select(s => new { Item = s.Key, Count = s.Count() }).ToDictionary(g => g.Item, g => g.Count);
+		}
+	}
+}
